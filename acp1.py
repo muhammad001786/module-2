2fedab1a49f72784579bfede6ad9133a7c470700
muhammad001786 <aqsa.jawed33@gmail.com>
2025-07-22 01:43:19 -0700

    commit2

---
+++ acp1.py
@@ -0,0 +1,5 @@
+name =input("enter your name: ")
+greet ="congratulations " + name + "!"
+print(greet.upper())
+print(greet.lower())
+print(greet.capitalize())
---
+++ average.py
@@ -0,0 +1,12 @@
+#storing values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 95
+tree5 = 11
+#Finding the total of trees
+sum = tree1+tree2+tree3+tree4+tree5
+print("the sum of all 5 trees is: ", sum)
+#Finding the average of trees
+average = sum/s
+print("the average of all the tree is :", average)
