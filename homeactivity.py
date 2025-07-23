746a20f42989e18566266a5b14541810c45de584
muhammad001786 <aqsa.jawed33@gmail.com>
2025-07-23 01:25:05 -0700

    commit3

---
+++ homeactivity.py
@@ -0,0 +1,4 @@
+print("square root of a number")
+number = int(input("enter a number: "))
+square_root=number**0.5
+print("square root of",number,"is",square_root)
---
+++ if.py
@@ -0,0 +1,8 @@
+print("if statement .")
+number= int(input("enter a number: "))
+if number>0:
+  print("number is positive")
+if number<0:
+  print("number is negative")
+if number==0:
+  print("number is zero itself")
