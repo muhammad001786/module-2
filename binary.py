700335c3b1fc2069e4b84257a324a56d24798f51
muhammad001786 <aqsa.jawed33@gmail.com>
2025-07-30 01:34:35 -0700

    commit2

---
+++ binary.py
@@ -0,0 +1,13 @@
+print("Welcome to the Bitwise Magic Show!")
+a = 4    #Binary: 100
+b = 5    #Binary: 101
+print("Let's see how numbers work in binary: ")
+print(f"a (a) -> (bin(a))") 
+print(f"b (b) -> (bin(b)]")
+print("\nBitwise AND (a & b):")
+print(f"(a) & (b) = (a & b) -> {bin(a & b)})
+print("\nBitwise OR (ab):")
+print(f"(a) (b) = (ab) -> (bin(a | b))") print("\nBitwise NOT (-a):")
+print(f"~(a)=(-a) -> (bin(-a))")
+print("\nBitwise XOR (ab):")
+print(f"(a)(b)= (a^b) -> (bin(ab)]")
